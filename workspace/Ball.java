1ae4ca36e404ab84f0a19c309d424557b93afce9
cSilva1408 <cardiesil14@gmail.com>
2024-12-07 01:24:18 +0000

    commit -m “<submission on 12/6>”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,127 @@
 import java.awt.*;
 
 public class Ball {
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
+	// Instance variables to represent the position, size, and velocity of the ball
+	private int xPos;
+	private int yPos;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
 
+	// Constructor to initialize the ball's position and size
+	//
+	//  Class author: Carlos Silva
+	//  Date created: December 6th 2024
+	//  General description: This class represents a paddle in a game, with methods to control its movement and render it on the screen.
+	//
+	//  Pre-condition: The input values for `xPos`, `yPos`, and `size` must be non-negative integers. 
+	//  `size` should represent the diameter of the ball.
+	//  Post-condition: A Ball object is created with the specified position and size. Velocity defaults to 0.
+	//
+	public Ball(int xPos, int yPos, int size) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.size = size;
+	}
+
+	// Method to retrieve the x-coordinate of the ball
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current x-coordinate of the ball.
+	//
+	public int getXpos() {
+		return xPos;
+	}
+
+	// Method to retrieve the y-coordinate of the ball
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current y-coordinate of the ball.
+	//
+	public int getYpos() {
+		return yPos;
+	}
+
+	// Method to retrieve the size (diameter) of the ball
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the diameter of the ball.
+	//
+	public int getSize() {
+		return size;
+	}
+
+	// Method to reverse the ball's vertical direction
+	//
+	//  Pre-condition: The ball must have a non-zero vertical velocity.
+	//  Post-condition: The ball's vertical velocity (`yVelocity`) is reversed.
+	//
+	public void reverseY() {
+		yVelocity = yVelocity * -1;
+	}
+
+	// Method to reverse the ball's horizontal direction
+	//
+	//  Pre-condition: The ball must have a non-zero horizontal velocity.
+	//  Post-condition: The ball's horizontal velocity (`xVelocity`) is reversed.
+	//
+	public void reverseX() {
+		xVelocity = xVelocity * -1;
+	}
+
+	// Method to set the ball's horizontal velocity
+	//
+	//  Pre-condition: The input `number` must be an integer representing the new x-velocity.
+	//  Post-condition: The ball's horizontal velocity (`xVelocity`) is updated to the specified value.
+	//
+	public void setXVelocity(int number) {
+		xVelocity = number;
+	}
+
+	// Method to set the ball's vertical velocity
+	//
+	//  Pre-condition: The input `number` must be an integer representing the new y-velocity.
+	//  Post-condition: The ball's vertical velocity (`yVelocity`) is updated to the specified value.
+	//
+	public void setYVelocity(int number) {
+		yVelocity = number;
+	}
+
+	// Method to draw the ball on a graphical surface
+	//
+	//  Pre-condition: The `Graphics` object `g` must be non-null and linked to a valid drawing canvas.
+	//  Post-condition: The ball is drawn as a filled oval at its current position with its size and color.
+	//
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+        g.fillOval(xPos, yPos, size, size);
+	}
+
+	// Method to move the ball based on its velocity
+	//
+	//  Pre-condition: The ball's velocity (`xVelocity` and `yVelocity`) must be initialized.
+	//  Post-condition: The ball's position (`xPos` and `yPos`) is updated by adding the velocity to its current coordinates.
+	//
+	public void move() {
+		xPos += xVelocity;
+		yPos += yVelocity;
+	}
+
+	// Method to set the ball's x-coordinate
+	//
+	//  Pre-condition: The input `number` must be a valid integer representing the new x-coordinate.
+	//  Post-condition: The ball's x-coordinate (`xPos`) is updated to the specified value.
+	//
+	public void setX(int number) {
+		xPos = number;
+	}
+
+	// Method to set the ball's y-coordinate
+	//
+	//  Pre-condition: The input `number` must be a valid integer representing the new y-coordinate.
+	//  Post-condition: The ball's y-coordinate (`yPos`) is updated to the specified value.
+	//
+	public void setY(int number) {
+		yPos = number;
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,118 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
-	
-	
+	// Instance variables to represent the position, size, and velocity of the paddle
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int velocity;
+
+	// Constructor to initialize the paddle's position, width, and height
+	//
+	//  Class author: Carlos Silva
+	//  Date created: December 6th 2024
+	//  General description: This class represents a paddle in a game, with methods to control its movement and render it on the screen.
+	//
+	//  Pre-condition: The input values for `x`, `y`, `width`, and `height` must be positive integers.
+	//  Post-condition: A Paddle object is created with the specified position, width, and height. Velocity defaults to 0.
+	//
+	public Paddle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	// Method to retrieve the x-coordinate of the paddle
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current x-coordinate of the paddle.
+	//
+	public int getX() {
+		return x;
+	}
+
+	// Method to retrieve the y-coordinate of the paddle
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current y-coordinate of the paddle.
+	//
+	public int getY() {
+		return y;
+	}
+
+	// Method to retrieve the width of the paddle
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current width of the paddle.
+	//
+	public int getWidth() {
+		return width;
+	}
+
+	// Method to retrieve the height of the paddle
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current height of the paddle.
+	//
+	public int getHeight() {
+		return height;
+	}
+
+	// Method to retrieve the velocity of the paddle
+	//
+	//  Pre-condition: None
+	//  Post-condition: Returns the current velocity of the paddle.
+	//
+	public int getVelocity() {
+		return velocity;
+	}
+
+	// Method to move the paddle horizontally based on its velocity
+	//
+	//  Pre-condition: The paddle's velocity (`velocity`) must be initialized.
+	//  Post-condition: The x-coordinate of the paddle is updated by adding the velocity to its current value.
+	//
+	public void move() {
+		x = x + velocity;
+	}
+
+	// Method to update the paddle's velocity
+	//
+	//  Pre-condition: The input `number` must be an integer representing the new velocity.
+	//  Post-condition: The paddle's velocity (`velocity`) is updated to the specified value.
+	//
+	public void addVelocity(int number) {
+		velocity = number;
+	}
+
+	// Method to set the x-coordinate of the paddle
+	//
+	//  Pre-condition: The input `number` must be a valid integer representing the new x-coordinate.
+	//  Post-condition: The x-coordinate of the paddle is updated to the specified value.
+	//
+	public void setX(int number) {
+		x = number;
+	}
+
+	// Method to set the velocity of the paddle
+	//
+	//  Pre-condition: The input `number` must be an integer representing the new velocity.
+	//  Post-condition: The paddle's velocity (`velocity`) is updated to the specified value.
+	//
+	public void setVelocity(int number) {
+		velocity = number;
+	}
+
+	// Method to draw the paddle on a graphical surface
+	//
+	//  Pre-condition: The `Graphics` object `g` must be non-null and linked to a valid drawing canvas.
+	//  Post-condition: The paddle is drawn as a filled rectangle at its current position with its width and height.
+	//
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, y, width, height);
+	}
 }
